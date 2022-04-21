54ff35f05461e9de31b10adc7debe8fd8c58ae30
Marcos Campos <mvocampos@yahoo.com.br>
2022-04-21 15:27:57 -0300

    adicionando testes de UI

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -1,12 +1,19 @@
 package br.com.alura.leilao.ui.activity;
 
+import android.content.Intent;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.io.IOException;
+
+import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.model.Leilao;
 
 import static org.junit.Assert.*;
 
@@ -15,11 +22,26 @@ public class ListaLeilaoTelaTest {
 
     @Rule
     public ActivityTestRule activity =
-            new ActivityTestRule(ListaLeilaoActivity.class, true, true);
+            new ActivityTestRule(ListaLeilaoActivity.class, true, false);
 
     @Test
-    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaApi() {
-        Espresso.onView(ViewMatchers.withText("moto"))
+    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaApi() throws IOException {
+        LeilaoWebClient webClient = new LeilaoWebClient();
+
+        boolean bancoDeDadosNaoFoiLimpo = !webClient.limpaBancoDeDados();
+
+        if (bancoDeDadosNaoFoiLimpo){
+            Assert.fail("Banco de dados não foi limpo");
+        }
+
+        Leilao carroSalvo = webClient.salva(new Leilao("Carro"));
+        if (carroSalvo == null){
+            Assert.fail("Leilão não foi salvo");
+        }
+
+        activity.launchActivity(new Intent());
+
+        Espresso.onView(ViewMatchers.withText("Carro"))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 
