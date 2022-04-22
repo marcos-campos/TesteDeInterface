f21fcbdbd877cc49cc247238690f465941832152
Marcos Campos <mvocampos@yahoo.com.br>
2022-04-21 22:22:47 -0300

    adicionando matches para caso especifico

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -1,11 +1,15 @@
 package br.com.alura.leilao.ui.activity;
 
 import android.content.Intent;
-import android.support.test.espresso.Espresso;
 import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
+import org.hamcrest.Description;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,8 +20,11 @@ import java.io.IOException;
 
 import br.com.alura.leilao.R;
 import br.com.alura.leilao.api.retrofit.client.TestWebClient;
+import br.com.alura.leilao.formatter.FormatadorDeMoeda;
 import br.com.alura.leilao.model.Leilao;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.core.AllOf.allOf;
 
@@ -27,6 +34,7 @@ public class ListaLeilaoTelaTest {
     public ActivityTestRule activity =
             new ActivityTestRule(ListaLeilaoActivity.class, true, false);
     private final TestWebClient webClient = new TestWebClient();
+    private final FormatadorDeMoeda formatadorDeMoeda = new FormatadorDeMoeda();
 
     @Before
     public void setup() throws IOException {
@@ -39,9 +47,15 @@ public class ListaLeilaoTelaTest {
 
         activity.launchActivity(new Intent());
 
-        Espresso.onView(allOf(ViewMatchers.withText("Carro"),
+        onView(allOf(ViewMatchers.withText("Carro"),
                 withId(R.id.item_leilao_descricao)))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+                .check(matches(ViewMatchers.isDisplayed()));
+
+        String formatoEsperado = formatadorDeMoeda.formata(0.0);
+
+        onView(allOf(ViewMatchers.withText(formatoEsperado),
+                withId(R.id.item_leilao_maior_lance)))
+                .check(matches(ViewMatchers.isDisplayed()));
     }
 
     @Test
@@ -50,13 +64,52 @@ public class ListaLeilaoTelaTest {
 
         activity.launchActivity(new Intent());
 
-        Espresso.onView(allOf(ViewMatchers.withText("Carro"),
-                withId(R.id.item_leilao_descricao)))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
 
-        Espresso.onView(allOf(ViewMatchers.withText("Computador"),
-                withId(R.id.item_leilao_descricao)))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+//        Espresso.onView(allOf(ViewMatchers.withText("Carro"),
+//                withId(R.id.item_leilao_descricao)))
+//                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+//
+//        String formatoEsperadoCarro = formatadorDeMoeda.formata(0.0);
+//
+//        Espresso.onView(allOf(ViewMatchers.withText(formatoEsperadoCarro),
+//                withId(R.id.item_leilao_maior_lance)))
+//                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+//
+//
+//        Espresso.onView(allOf(ViewMatchers.withText("Computador"),
+//                withId(R.id.item_leilao_descricao)))
+//                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+//
+//        String formatoEsperadoComputador = formatadorDeMoeda.formata(0.0);
+//
+//        Espresso.onView(allOf(ViewMatchers.withText(formatoEsperadoComputador),
+//                withId(R.id.item_leilao_maior_lance)))
+//                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .check(matches(apareceLeilao(0, "Carro", 0.00)));
+    }
+
+    private BoundedMatcher<View, RecyclerView> apareceLeilao(final int posicao,
+                                                             final String descricaoEsperada,
+                                                             final double maiorLanceEsperado) {
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+
+            }
+
+            @Override
+            protected boolean matchesSafely(RecyclerView item) {
+                View viewDoViewHolder = item.findViewHolderForAdapterPosition(posicao).itemView;
+                TextView textViewDescricao = viewDoViewHolder.findViewById(R.id.item_leilao_descricao);
+                boolean temDescricaoEsperada = textViewDescricao.getText().toString().equals(descricaoEsperada);
+                TextView textViewMaiorLance = viewDoViewHolder.findViewById(R.id.item_leilao_maior_lance);
+                FormatadorDeMoeda formatador = new FormatadorDeMoeda();
+                boolean temMaiorLanceEsperado = textViewMaiorLance.getText().toString().equals(formatador.formata(maiorLanceEsperado));
+                return temDescricaoEsperada && temMaiorLanceEsperado;
+            }
+        };
     }
 
     @After
